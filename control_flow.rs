32f52d4ea95d3452a695d920fcd78981b15a5d02
Satyam Sherkar <satyamsherkar007@gmail.com>
2025-12-20 19:27:08 +0530

    a little about loop and if

---
+++ control_flow.rs
@@ -0,0 +1,24 @@
+// Basics of conditionals and loops in Rust
+
+// if statement
+
+// fn main() {
+//     let number = 8;
+
+//     if number < 7 {
+//         println!("Number is less than 7");
+//     } else if number == 7 {
+//         println!("Number is 8");
+//     } else {
+//         println!("Number is greater then 7");
+//     }
+// }
+
+
+// A continuous loop
+
+// fn main() {
+//     loop {
+//         println!("again!");
+//     }
+// }
