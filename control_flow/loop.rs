21ebd20350c93c7fc8363bebadd3c185cede850d
Satyam Sherkar <satyamsherkar007@gmail.com>
2025-12-21 12:36:40 +0530

    returning a value from loop

--- control_flow/loop.rs
+++ control_flow/loop.rs
@@ -1,7 +1,20 @@
 // A continuous loop
 
+// fn main() {
+//     loop {
+//         println!("again!");
+//     }
+// }
+
+// Returning a value from a loop
+
 fn main() {
-    loop {
-        println!("again!");
-    }
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+        break counter * 2; // this line stops the loop and returns the value after the break keyword
+        }
+    };
+    println!("The result is {result}");
 }
