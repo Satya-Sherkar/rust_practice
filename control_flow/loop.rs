968e89185650ca70d465f49acc74074215515e78
Satyam Sherkar <satyamsherkar007@gmail.com>
2025-12-21 22:04:04 +0530

    completed loops in rust

--- control_flow/loop.rs
+++ control_flow/loop.rs
@@ -1,13 +1,11 @@
 // A continuous loop
-
-// fn main() {
-//     loop {
-//         println!("again!");
-//     }
-// }
+fn main() {
+    loop {
+        println!("again!"); // infinite loop
+    }
+}
 
 // Returning a value from a loop
-
 fn main() {
     let mut counter = 0;
     let result = loop {
@@ -17,4 +15,46 @@ fn main() {
         }
     };
     println!("The result is {result}");
+}
+
+// Loop in loop
+fn main() {
+    let mut count = 0;
+    // labeled loop syntax
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+
+// while loop
+fn main() {
+    let mut number = 4;
+
+    while number > 0 {
+        println!("{number}");
+        number -= 1;
+    }
+    println!("Done");
+}
+
+// for loop
+fn main() {
+    let a = [10, 20, 30, 40, 50];
+    for element in a {
+        println!("the value is: {element}");
+    }
 }
