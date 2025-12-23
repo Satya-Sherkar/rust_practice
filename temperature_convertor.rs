0332df8a907604aae131cf7e4022c1215f13fe61
Satyam Sherkar <satyamsherkar007@gmail.com>
2025-12-23 15:02:44 +0530

    invalid input handling with loop

--- temperature_convertor.rs
+++ temperature_convertor.rs
@@ -1,20 +1,33 @@
 use std::io;
 
-fn convert_temp(temp_in_celcius: i32) -> i32 {
-    let temp_in_fahrenheit = (temp_in_celcius * 9 / 5) + 32;
+fn convert_temp(temp_in_celsius: i32) -> i32 {
+    let temp_in_fahrenheit = (temp_in_celsius * 9 / 5) + 32;
     return temp_in_fahrenheit;
 }
 
 fn main() {
-    println!("Convert Celcius into Fahrenheit!!");
+    println!("Convert Celsius into Fahrenheit!!");
+    println!("Enter temperature in celsius");
     let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read lines");
+    loop {
+        input.clear();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read lines");
 
-    let temp_in_celcius: i32 = input.trim().parse().expect("Failed to parse");
+        match input.trim().parse::<i32>() {
+            Ok(n) => {
+                let temp_in_celsius: i32 = n;
 
-    let temp_in_fahrenheit = convert_temp(temp_in_celcius);
+                let temp_in_fahrenheit = convert_temp(temp_in_celsius);
 
-    println!("Temperature in fahrenheit: {temp_in_fahrenheit}");
+                println!("Temperature in fahrenheit: {temp_in_fahrenheit}");
+
+                break;
+            }
+            Err(e) => {
+                println!("Invalid input: {}. Please enter a valid integer.", e);
+            }
+        }
+    }
 }
